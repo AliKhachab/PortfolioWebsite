c3d94b4df69047c1b1f2a1ba8f30a4c619c82a6d
AliKhachab <a.khachab620@gmail.com>
2023-12-31 18:08:07 -0500

    project styling halfway done

--- components/projects.tsx
+++ components/projects.tsx
@@ -29,8 +29,8 @@ type ProjectProps = (typeof projectsData)[number]; // number = iterate through a
 
 function Project({ title, description, tags, imageUrl } : ProjectProps) {
     return (
-        <section className="bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative">
-            <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] sm:h-[20rem] mb-3 sm:mb-8 flex flex-col h-full last:mb-0">
+        <section className="group relative bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 even:pl-8 hover:bg-gray-200 transition">
+            <div className="p5-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
             <h3 className="text-2xl font-semibold">{title}</h3>
             <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
             <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
@@ -39,7 +39,11 @@ function Project({ title, description, tags, imageUrl } : ProjectProps) {
             ))
             } </ul>
             </div>
-            <Image src={imageUrl} alt="Project I worked on." quality={95} className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl"/>
+            <Image src={imageUrl} 
+            alt="Project I worked on." 
+            quality={95} 
+            className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-even:-right-[initial] group-even:-left-40 group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 transition group-hover:scale-105
+            group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2"/>
         </section>
     );
 }
