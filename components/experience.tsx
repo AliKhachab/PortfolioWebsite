4070d2e415c1dd4b632b908c59e49a7c9009fb6e
AliKhachab <a.khachab620@gmail.com>
2024-01-04 18:46:20 -0500

    experience done

--- components/experience.tsx
+++ components/experience.tsx
@@ -5,20 +5,20 @@ import SectionHeading from "./section-heading";
 import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import { experiencesData } from '@/lib/data';
-import { brotliDecompress } from 'zlib';
 
 
 export default function Experience() {
 
-  const { ref } = useSectionInView("Experience");
+  const { ref } = useSectionInView("Experience", 0.7);
 
   return (
     <section id="experience" ref={ ref }>
         <SectionHeading>My experience</SectionHeading>
-        <VerticalTimeline>
+        <VerticalTimeline lineColor="">
             {experiencesData.map((item, index) => (
                 <React.Fragment key={index}>
                     <VerticalTimelineElement
+                    visible = {true}
                     contentStyle={{
                         background: "#f3f4f6",
                         boxShadow: "none",
@@ -27,11 +27,17 @@ export default function Experience() {
                         textAlign: "left",
                     }}
                     contentArrowStyle={{
-                        borderRight: "0.4rem solid #9ca3af"
+                        borderRight: "0.4rem solid #9ca3af",
+                    }}
+                    date={item.date}
+                    icon={item.icon}
+                    iconStyle={{
+                        background: "white",
+                        fontSize: "1.5rem"
                     }}>
-                        <h3>{item.title}</h3>
-                        <p>{item.location}</p>
-                        <p>{item.description}</p>
+                        <h3 className="font-semibold capitalize">{item.title}</h3>
+                        <p className="font-normal !mt-0">{item.location}</p>
+                        <p className="!mt-1 !font-normal text-gray-700">{item.description}</p>
                     </VerticalTimelineElement>
                 </React.Fragment>
             ))}
