fe2a254e2d847a4d77ba02c35676ec31d2220552
AliKhachab <a.khachab620@gmail.com>
2024-01-10 00:36:34 -0500

    up to loading indicator

--- components/contact.tsx
+++ components/contact.tsx
@@ -3,11 +3,13 @@ import React from "react";
 import { motion } from "framer-motion";
 import SectionHeading from "./section-heading";
 import { useSectionInView } from "@/lib/hooks";
-import { FaPaperPlane } from "react-icons/fa";
 import { sendEmail } from "@/actions/sendEmail";
+import SubmitBtn from "./submit-btn";
+
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact", 0.75);
+
 
   return (
     <motion.section
@@ -59,13 +61,7 @@ export default function Contact() {
           required
           maxLength={5000}
         />
-        <button
-          type="submit"
-          className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 active:scale-105 hover:bg-gray-950"
-        >
-          Submit{" "}
-          <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />{" "}
-        </button>
+        <SubmitBtn />
       </form>
     </motion.section>
   );
